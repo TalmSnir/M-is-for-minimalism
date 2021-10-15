f3c99a9c416f61b5b78392655b319f577c1414aa
TalmSnir <talmsnir@gmail.com>
2021-10-15 12:42:13 +0300

    added freechapter page

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
-const slides = [...document.querySelectorAll('.slide')];
-const sliderForwardBtn = document.querySelector(
-  '.slider_control.slider_controls_forward'
-);
-const sliderBackwardBtn = document.querySelector(
-  '.slider_control.slider_controls_back  '
-);
-const sliderPositionCircles = [
-  ...document.querySelectorAll('.slider_position>span'),
-];
 const menuBtn = document.querySelector('.header_menu_btn');
 const headerNav = document.querySelector('.header_nav');
+if (document.title === 'index') {
+  const slides = [...document.querySelectorAll('.slide')];
+  const sliderForwardBtn = document.querySelector(
+    '.slider_control.slider_controls_forward'
+  );
+  const sliderBackwardBtn = document.querySelector(
+    '.slider_control.slider_controls_back  '
+  );
+  const sliderPositionCircles = [
+    ...document.querySelectorAll('.slider_position>span'),
+  ];
 
-let i = 0;
-const interval = setInterval(() => {
-  nextSlide();
-}, 2000);
+  let i = 0;
+  const interval = setInterval(() => {
+    nextSlide();
+  }, 2000);
 
-function nextSlide() {
-  slides[i].classList.remove('active');
-  sliderPositionCircles[i].classList.remove('active');
-  i = (i + 1) % slides.length;
-  slides[i].classList.add('active');
-  sliderPositionCircles[i].classList.add('active');
-}
+  function nextSlide() {
+    slides[i].classList.remove('active');
+    sliderPositionCircles[i].classList.remove('active');
+    i = (i + 1) % slides.length;
+    slides[i].classList.add('active');
+    sliderPositionCircles[i].classList.add('active');
+  }
 
-function prevSlide() {
-  slides[i].classList.remove('active');
-  sliderPositionCircles[i].classList.remove('active');
-  i = (i - 1 + slides.length) % slides.length;
-  slides[i].classList.add('active');
+  function prevSlide() {
+    slides[i].classList.remove('active');
+    sliderPositionCircles[i].classList.remove('active');
+    i = (i - 1 + slides.length) % slides.length;
+    slides[i].classList.add('active');
 
-  sliderPositionCircles[i].classList.add('active');
+    sliderPositionCircles[i].classList.add('active');
+  }
+
+  sliderForwardBtn.addEventListener('click', () => {
+    interval ? clearInterval(interval) : null;
+    nextSlide();
+  });
+  sliderBackwardBtn.addEventListener('click', () => {
+    interval ? clearInterval(interval) : null;
+    prevSlide();
+  });
 }
 
 function toggleHeaderNav() {
@@ -41,11 +52,3 @@ function closeHeaderNav(e) {
 }
 menuBtn.addEventListener('click', toggleHeaderNav);
 headerNav.addEventListener('click', closeHeaderNav);
-sliderForwardBtn.addEventListener('click', () => {
-  interval ? clearInterval(interval) : null;
-  nextSlide();
-});
-sliderBackwardBtn.addEventListener('click', () => {
-  interval ? clearInterval(interval) : null;
-  prevSlide();
-});
